d4ff60a37074dec9c05ac570213945af9b743c58
eltafarki <156535482+eltafarki@users.noreply.github.com>
2024-02-25 07:52:40 -0800

    Add files via upload

---
+++ time.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+ contract time{
+
+  
+  
+  uint public Twominute = block.timestamp + 2 minutes;
+  uint public Tenminute = block.timestamp + 10 minutes;
+ }
